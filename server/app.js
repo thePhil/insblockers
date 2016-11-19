948652198dc3b87b50134af057dc58873b92040c
Philipp Tölle <philipptoelle@gmail.com>
2016-11-19 18:41:57 +0100

    last cleanup

--- server/app.js
+++ server/app.js
@@ -12,45 +12,7 @@ var Server = require('./server');
 // and find the docker machine name you are using (usually default or eris).
 // for example, if the URL returned by docker-machine is tcp://192.168.99.100:2376
 // then your erisdbURL should be http://192.168.99.100:1337/rpc
-var erisdbURL = "http://192.168.99.100:2376/rpc";
 
 // get the abi and deployed data squared away
-var contractData = require('../jobs_output.json');
-
-
-var idisContractAddress = contractData["Root"];
-var idisAbi = JSON.parse(fs.readFileSync(path.join(__dirname, "../abi/" + idisContractAddress)));
-
-// properly instantiate the contract objects manager using the erisdb URL
-// and the account data (which is a temporary hack)
-var accountData = require('../accounts.json');
-var contractsManager = erisC.newContractManagerDev(erisdbURL, accountData.simplechain_full_000);
-
-// properly instantiate the contract objects using the abi and address
-var idisContract = contractsManager.newContractFactory(idisAbi).at(idisContractAddress);
-
-// display the current value of idi's contract by calling
-// the `get` function of idi's contract
-function getValue(callback) {
-  idisContract.getHello(function(error, result){
-    if (error) {
-      console.log(error, result);
-      throw error
-    }
-    console.log("Idi's number is:\t\t\t" + result.toNumber());
-    callback();
-  });
-}
-
-// prompt the user to change the value of idi's contract
-function changeValue() {
-	console.log("Done");
-}
-
-
-// run
-getValue(changeValue);
-
-
 // start server
 Server();
