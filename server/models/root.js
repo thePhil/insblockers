cf75fd623bc8ce5c42861a016a3e680422979e1f
Philipp Tölle <philipptoelle@gmail.com>
2016-11-19 12:21:35 +0100

    Added model file for root contract including functions to register

---
+++ server/models/root.js
@@ -0,0 +1,54 @@
+var fs = require ('fs');
+var erisC = require('eris-contracts');
+
+var erisdbURL = "http://localhost:1337/rpc";
+
+var contractData = require('../../jobs_output.json');
+
+var moduleName = module.filename.slice(__filename.lastIndexOf(path.sep)+1, module.filename.length -3);
+
+console.log(moduleName);
+
+var ctrAddress = contractData[moduleName];
+
+console.log(ctrAddress)
+
+var ctrABI = JSON.parse(fs.readFileSync("../../abi/" + ctrAddress));
+
+var accountData = require("../../accounts.json");
+var ctrManager = erisC.newContractManagerDev(erisdbURL, accountData.simplechain_full_000);
+
+var rootCtr = ctrManager.newContractFactory(ctrABI).at(ctrAddress);
+
+function registerInsurer(callback){
+	rootCtr.registerInsurer(function (error, result){
+		if (error) {
+			console.log(error, result);
+			throw error
+		}
+		console.log("Registered Insurer");
+		callback();
+	});
+}
+
+function registerRetailer(callback){
+	rootCtr.registerRetailer(function (error, result){
+		if (error) {
+			console.log(error, result);
+			throw error
+		}
+		console.log("Registered Retailer");
+		callback();
+	});
+}
+
+function registerServiceAgent(callback){
+	rootCtr.registerServiceAgent(function (error, result){
+		if (error) {
+			console.log(error, result);
+			throw error
+		}
+		console.log("Registered Service Agent.");
+		callback();
+	});
+}
