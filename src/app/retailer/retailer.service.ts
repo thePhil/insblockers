f8e636be66bea316c831eceea5f6936843c15c27
Alex Wilcox <alex.wilcox87@gmail.com>
2016-11-19 17:02:51 +0100

    register retailer button

--- src/app/retailer/retailer.service.ts
+++ src/app/retailer/retailer.service.ts
@@ -18,4 +18,9 @@ export class RetailerService {
 
   }
 
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
 }
